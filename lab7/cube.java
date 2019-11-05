d459c6ab176f044097c8df7e6f83a36ba19a9a5d
Jesus Hernandez <9a78jwDtSn3pYmZ>
2019-11-04 21:30:56 -0800

    cube.java added

--- lab7/cube.java
+++ lab7/cube.java
@@ -1,5 +1,20 @@
-class cube{
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+
+public class cube extends Application{
     public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage){
+        // Constants for the scene size    
+        final double SCENE_WIDTH = 320.0;
+        final double SCENE_HEIGHT = 240.0;
         
+Rectangle back_rectangle = new Rectangle(200,100,50,50);
+ 
     }
 }
